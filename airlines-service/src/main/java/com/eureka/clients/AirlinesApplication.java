9bae83317e58609e3848e94c38bb51778a82cb09
E1000\30044840 <javier.monje@ext.leroymerlin.es>
2018-07-12 14:38:06 +0200

    actualizado host

--- airlines-service/src/main/java/com/eureka/clients/AirlinesApplication.java
+++ airlines-service/src/main/java/com/eureka/clients/AirlinesApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration //defines this as a Spring Boot application
-@EnableDiscoveryClient //this enables service registration and discovery. In this case, this process registers itself with the discovery-server service using its application name 
+@EnableEurekaClient //this enables service registration and discovery. In this case, this process registers itself with the discovery-server service using its application name 
 @SpringBootApplication
 public class AirlinesApplication {
 	public static void main(String[] args) {
