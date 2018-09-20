e139d55f293e0b47c13d00b3e0d149c2aabdf90e
E1000\30044840 <javier.monje@ext.leroymerlin.es>
2018-09-20 17:15:29 +0200

    mod host config

--- airlines-service/src/main/java/com/eureka/clients/controller/ExampleCouchbase.java
+++ airlines-service/src/main/java/com/eureka/clients/controller/ExampleCouchbase.java
@@ -19,8 +19,8 @@ public class ExampleCouchbase {
 	private static Cluster cluster;
 	private static final String Q_AIRLINES = "SELECT id, name, country FROM `travel-sample` WHERE type = 'airline'";
 	private static Bucket getBucket(String bName, String bPass) {
-		cluster = CouchbaseCluster.create("192.168.99.102:8091");
-		//cluster = CouchbaseCluster.create(System.getenv("couchbase_addr"));
+		//cluster = CouchbaseCluster.create("192.168.99.102:8091");
+		cluster = CouchbaseCluster.create(System.getenv("couchbase_addr"));
 		return cluster.openBucket(bName, bPass);
 	}
 
