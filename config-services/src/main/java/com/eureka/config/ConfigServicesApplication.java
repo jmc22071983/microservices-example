393fe7d26a030bb0d6221f9b0d74f06fb9fe7ff5
E1000\30044840 <javier.monje@ext.leroymerlin.es>
2018-09-20 16:36:58 +0200

    mod yml

--- config-services/src/main/java/com/eureka/config/ConfigServicesApplication.java
+++ config-services/src/main/java/com/eureka/config/ConfigServicesApplication.java
@@ -1,14 +1,17 @@
 package com.eureka.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServicesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServicesApplication.class, args);
 	}
 }
+
+
