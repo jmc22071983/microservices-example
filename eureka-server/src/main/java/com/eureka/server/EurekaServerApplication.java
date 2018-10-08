b999e4aa7eca946fa41610998ff260b1aba24f5b
E1000\30044840 <javier.monje@ext.leroymerlin.es>
2018-10-08 17:31:07 +0200

    mod

--- eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
@@ -18,6 +19,7 @@ import com.netflix.appinfo.AmazonInfo;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaServerApplication {
 	private static final Logger logger = LoggerFactory.getLogger(EurekaServerApplication.class);
 
@@ -32,59 +34,20 @@ public class EurekaServerApplication {
 	@Bean
 	@Autowired
 	@Profile("aws")
-	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
-		/*
-		 * logger.info("*** Init eurekaInstanceConfigBean ***");
-		 * EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
-		 * AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-		 * config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
-		 * config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
-		 * logger.info("*** LOCAL HOSTNAME:" +
-		 * info.get(AmazonInfo.MetaDataKey.localHostname)); logger.info("*** LOCAL IP:"
-		 * + info.get(AmazonInfo.MetaDataKey.localIpv4));
-		 * logger.info("*** PUBLIC HOSTNAME:" +
-		 * info.get(AmazonInfo.MetaDataKey.publicHostname));
-		 * logger.info("*** PUBLIC IP:" + info.get(AmazonInfo.MetaDataKey.publicIpv4));
-		 * logger.info("*** availabilityZone:" +
-		 * info.get(AmazonInfo.MetaDataKey.availabilityZone));
-		 * config.setNonSecurePort(port); config.setDataCenterInfo(info); return config;
-		 */
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {		
 		logger.info("*** Init eurekaInstanceConfigBean ***");
-		final EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(inetUtils) {
-			@Scheduled(initialDelay = 30000L, fixedRate = 30000L)
-			public void refreshInfo() {
-				AmazonInfo newInfo = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-				if (!this.getDataCenterInfo().equals(newInfo)) {
-					logger.info("*** ! this.getDataCenterInfo().equals(newInfo) ***");
-					((AmazonInfo) this.getDataCenterInfo()).setMetadata(newInfo.getMetadata());
-					logger.info("*** Setting newInfo properties ***");
-					this.setHostname(newInfo.get(AmazonInfo.MetaDataKey.publicHostname));
-					this.setIpAddress(newInfo.get(AmazonInfo.MetaDataKey.publicIpv4));
-					this.setDataCenterInfo(newInfo);
-					this.setNonSecurePort(port);
-					logger.info("*** LOCAL HOSTNAME:" + newInfo.get(AmazonInfo.MetaDataKey.localHostname));
-					logger.info("*** PUBLIC IP:" + newInfo.get(AmazonInfo.MetaDataKey.publicIpv4));
-					logger.info("*** PUBLIC IP:" + newInfo.get(AmazonInfo.MetaDataKey.publicIpv4));
-				}
-			}
-		};
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
 		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-		logger.info("*** Setting Info properties ***");
-		instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
-		instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
-		instance.setDataCenterInfo(info);
-		instance.setNonSecurePort(port);
-		logger.info("*** LOCAL HOSTNAME:" + info.get(AmazonInfo.MetaDataKey.localHostname));
-		logger.info("*** PUBLIC IP:" + info.get(AmazonInfo.MetaDataKey.publicIpv4));
-		logger.info("*** PUBLIC IP:" + info.get(AmazonInfo.MetaDataKey.publicIpv4));
-
-		return instance;
+		config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+		config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		logger.info("LOCAL HOSTNAME {}, ", info.get(AmazonInfo.MetaDataKey.localHostname)); 
+		logger.info("LOCAL IP {} ", info.get(AmazonInfo.MetaDataKey.localIpv4));
+		logger.info("PUBLIC HOSTNAME {}, ", info.get(AmazonInfo.MetaDataKey.publicHostname));
+		logger.info("PUBLIC IP {}, ", info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		logger.info("availabilityZone {}, " , info.get(AmazonInfo.MetaDataKey.availabilityZone));
+		config.setNonSecurePort(port); config.setDataCenterInfo(info);
+		return config;
 	}
 	
-/*	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
-		  EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
-		  AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-		  b.setDataCenterInfo(info);
-		  return b;
-	}*/
+
 }
