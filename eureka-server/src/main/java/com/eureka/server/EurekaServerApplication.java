9a565cfcaaae613ea89746500a0cdce6845f24f5
E1000\30044840 <javier.monje@ext.leroymerlin.es>
2018-10-11 12:14:49 +0200

    mod

--- eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -8,12 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import com.netflix.appinfo.AmazonInfo;
 
