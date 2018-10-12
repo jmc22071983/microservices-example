ac3ed746d5c364b0f66b746c553db5c42ecbb32d
javier <javier.monje.cerrato@gmail.com>
2018-10-12 11:12:23 +0200

    mod

--- airlines-service/src/main/java/com/eureka/clients/AirlinesApplication.java
+++ airlines-service/src/main/java/com/eureka/clients/AirlinesApplication.java
@@ -1,11 +1,18 @@
 package com.eureka.clients;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @EnableEurekaClient //this enables service registration and discovery. In this case, this process registers itself with the discovery-server service using its application name 
 @SpringBootApplication
@@ -13,4 +20,16 @@ public class AirlinesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AirlinesApplication.class, args);
 	}
+	
+	@Bean
+	@Autowired
+	@Profile("aws")
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+		config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		config.setDataCenterInfo(info);
+		return config;
+}
 }
