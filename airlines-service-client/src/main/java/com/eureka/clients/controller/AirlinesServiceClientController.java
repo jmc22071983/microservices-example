a39f33a3a7057e2f5d9c00c509812883985face4
30044840 <javier.monje@ext.leroymerlin.es>
2019-01-14 16:06:48 +0100

    mod

--- airlines-service-client/src/main/java/com/eureka/clients/controller/AirlinesServiceClientController.java
+++ airlines-service-client/src/main/java/com/eureka/clients/controller/AirlinesServiceClientController.java
@@ -1,16 +1,25 @@
 package com.eureka.clients.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.eureka.clients.ClientAirlinesService;
 
 //@Controller For thymeleaf view
 @RestController
 public class AirlinesServiceClientController {
-
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
 	protected ClientAirlinesService airlineTest;
 
 	public AirlinesServiceClientController(ClientAirlinesService airlineTest) {
@@ -34,5 +43,43 @@ public class AirlinesServiceClientController {
 	public String allAirlines() {
 		return airlineTest.allAirlines();
 	}
+	
+	@RequestMapping(value="/service-instances/{applicationName}", method = RequestMethod.GET)
+	public List<ServiceInstance> serviceInstancesByApplicationName( @PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
+	
+	@RequestMapping(value="/showAllServiceIds",  method = RequestMethod.GET)
+	public String showAllServiceIds() {
+		List<String> serviceIds = this.discoveryClient.getServices();
+        if (serviceIds == null || serviceIds.isEmpty()) {
+            return "No services found!";
+        }
+        String html = "<h3>Service Ids:</h3>";
+        for (String serviceId : serviceIds) {
+            html += "<br><a href='showService?serviceId=" + serviceId + "'>" + serviceId + "</a>";
+        }
+        return html;
+	}
+	
+    @RequestMapping(value = "/showService", method = RequestMethod.GET)
+    public String showFirstService(@RequestParam(defaultValue = "") String serviceId) {
+ 
+        // (Need!!) eureka.client.fetchRegistry=true
+        List<ServiceInstance> instances = this.discoveryClient.getInstances(serviceId);
+ 
+        if (instances == null || instances.isEmpty()) {
+            return "No instances for service: " + serviceId;
+        }
+        String html = "<h2>Instances for Service Id: " + serviceId + "</h2>";
+ 
+        for (ServiceInstance serviceInstance : instances) {
+            html += "<h3>Instance: " + serviceInstance.getUri() + "</h3>";
+            html += "Host: " + serviceInstance.getHost() + "<br>";
+            html += "Port: " + serviceInstance.getPort() + "<br>";
+        }
+ 
+        return html;
+    }
 
 }
