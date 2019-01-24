1c9cbe7d8e92e23e5a2d26562bfff41c3aa840c5
javier <javier.monje.cerrato@gmail.com>
2019-01-24 10:53:20 +0100

    mod

---
+++ gateway-service/src/main/java/com/gateway/service/RouteFilter.java
@@ -0,0 +1,43 @@
+package com.gateway.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class RouteFilter extends ZuulFilter{
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public String filterType() {
+		return "route";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		RequestContext context = RequestContext.getCurrentContext();
+        HttpSession httpSession = context.getRequest().getSession();
+       // Session session = repository.getSession(httpSession.getId());
+        log.info("SESSION ID ROUTEFILTER: {}", httpSession.getId());
+        context.addZuulRequestHeader( "Cookie", "SESSION=" + httpSession.getId());
+        return null;
+
+		
+	}
+}
---
+++ gateway-service/src/main/java/com/gateway/service/ZuulPosFilter.java
@@ -0,0 +1,48 @@
+package com.gateway.service;
+
+import com.google.common.net.HttpHeaders;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class ZuulPosFilter extends ZuulFilter {
+	
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1000;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		RequestContext context = RequestContext.getCurrentContext();
+    	HttpServletResponse servletResponse = context.getResponse();
+    	String setCookie = servletResponse.getHeader(HttpHeaders.SET_COOKIE);
+    	
+    	String auth = servletResponse.getHeader(HttpHeaders.AUTHORIZATION);
+ 	
+		return null;
+	}
+}
